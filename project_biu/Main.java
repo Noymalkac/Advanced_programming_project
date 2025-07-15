efc3bbd9c3863356949641b3ea99b9d574c87053
Noymalkac <119338907+Noymalkac@users.noreply.github.com>
2025-07-15 18:49:01 +0300

    Update Main.java

--- project_biu/Main.java
+++ project_biu/Main.java
@@ -3,6 +3,10 @@ import server.MyHTTPServer;
 import servlets.ConfLoader;
 import servlets.HtmlLoader;
 import servlets.TopicDisplayer;
+import java.awt.Desktop;
+import java.net.URI;
+
+
 
 public class Main {
 public static void main(String[] args) throws Exception{
@@ -14,6 +18,11 @@ public static void main(String[] args) throws Exception{
     server.addServlet("GET", "/graph", new servlets.GraphDisplayer());
     server.addServlet("GET", "/table", new servlets.TableDisplayer());
     server.start();
+    // Automatically open browser on Windows
+    if (Desktop.isDesktopSupported()) {
+        Desktop.getDesktop().browse(new URI("http://localhost:8080/app/index.html"));
+    }
+
     System.in.read();
     server.close();
     System.out.println("done");
